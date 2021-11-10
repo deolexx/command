25b5833fe5d12b64079894e9601befc62d9b5bca
Serhii Lukianenko <lukyanenko88@gmail.com>
2021-11-10 11:37:54 +0200

    pom and connection configured for aws deployment v2

--- src/main/java/com/green/CommandServiceServer.java
+++ src/main/java/com/green/CommandServiceServer.java
@@ -3,15 +3,28 @@ package com.green;
 import com.green.service.UserServiceImplementation;
 
 import javax.xml.ws.Endpoint;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class CommandServiceServer {
     private static final Logger LOGGER = Logger.getGlobal();
 
     public static void main(String[] args) {
+        Endpoint.publish("http://0.0.0.0:8081/command", new UserServiceImplementation());
         LOGGER.info("Creating WEB server and publish SOAP endpoint");
-        Endpoint.publish("http://localhost:8081/command", new UserServiceImplementation());
 
+        Runnable helloRunnable = new Runnable() {
+            public void run() {
+                LOGGER.info("Command working");
+            }
+        };
+
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(helloRunnable, 0, 15, TimeUnit.SECONDS);
 
     }
+
+
 }
