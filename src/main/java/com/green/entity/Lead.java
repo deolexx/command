44198142f8240ecc4a80458b5944bb02d96ed7e0
Serhii Lukianenko <lukyanenko88@gmail.com>
2021-11-30 11:03:26 +0200

    findByRoleAndGroup fixed

--- src/main/java/com/green/entity/Lead.java
+++ src/main/java/com/green/entity/Lead.java
@@ -10,7 +10,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.FIELD)
 @Entity
 @DiscriminatorValue("lead")
-public class Lead extends User {
+public class Lead extends Student {
 
 
     public Lead() {
